2d9a3dcf5136f4c27e52029fb09b98fd69ac2861
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-04-30 13:59:07 -0400

    Update User.java

--- app/src/main/java/com/appdevteam/myapplication/data/User.java
+++ app/src/main/java/com/appdevteam/myapplication/data/User.java
@@ -1,20 +1,25 @@
 package com.appdevteam.myapplication.data;
 
 import com.google.firebase.auth.FirebaseUser;
-import org.json.JSONObject;
 
 public class User {
+    private String userId;
     private String userName;
     private String userEmail;
-    private JSONObject userCards;
+    private Library library;
 
     public User(FirebaseUser user){
+        this.userId = user.getUid();
         this.userName = user.getDisplayName();
         this.userEmail = user.getEmail();
+        this.library = new Library();
     }
 
+    public String userId() {
+        return userId;
+    }
 
-    public String GetUserName() {
+    public String getUserName() {
         return userName;
     }
 
@@ -22,11 +27,11 @@ public class User {
         return userEmail;
     }
 
-    public JSONObject getUserCards() {
-        return userCards;
+    public Library getUserCards() {
+        return library;
     }
 
-    public void setUserCards(JSONObject userCards){
-        this.userCards = userCards;
+    public void setUserCards(Library library){
+        this.library = library;
     }
 }
