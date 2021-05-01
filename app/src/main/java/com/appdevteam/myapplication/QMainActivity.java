74a3d71a4081f9e31974e43a0c540d0b90e88f06
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-01 14:25:16 -0400

    Update QMainActivity.java

--- app/src/main/java/com/appdevteam/myapplication/QMainActivity.java
+++ app/src/main/java/com/appdevteam/myapplication/QMainActivity.java
@@ -2,11 +2,9 @@ package com.appdevteam.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import com.appdevteam.myapplication.data.User;
+import com.appdevteam.myapplication.testing.DataTest;
 
 //import com.appdevteam.myapplication.data.Firebase;
 
@@ -17,14 +15,9 @@ public class QMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q_main);
-//        Firebase db = new Firebase();
-        Intent info = getIntent();
-        User cUser = (User) info.getSerializableExtra("cUser");
-        test = findViewById(R.id.textView);
-        test.setText(cUser.toMap().toString());
 
+        DataTest.testOnCreate(this, test);
 
-//        db.write("message");
 
     }
 }
