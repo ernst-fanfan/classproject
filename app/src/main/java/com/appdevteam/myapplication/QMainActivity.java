49c5105b87169a73194068bed9e05d0f32e66227
ernst <11448222+ernst-fanfan@users.noreply.github.com>
2021-05-01 16:25:38 -0400

    Update QMainActivity.java

--- app/src/main/java/com/appdevteam/myapplication/QMainActivity.java
+++ app/src/main/java/com/appdevteam/myapplication/QMainActivity.java
@@ -9,14 +9,16 @@ import com.appdevteam.myapplication.testing.DataTest;
 //import com.appdevteam.myapplication.data.Firebase;
 
 public class QMainActivity extends AppCompatActivity {
-    TextView test;
+    TextView test0;
+    TextView test1;
+    TextView test2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q_main);
 
-        DataTest.testOnCreate(this, test);
+        DataTest.testOnCreate(this, test0, test1, test2);
 
 
     }
